5c75c7f3b744d47ec3aaeefdf0717fad7ef0d440
Rob McCarthy <rmccarthy@bridgephase.com>
2025-03-17 00:24:55 -0400

    progress

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,17 @@
 FROM python:3.12
 COPY . /src
-RUN pip install kopf
-RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && chmod +x kubectl && mv kubectl /usr/bin/kubectl
-CMD kopf run /src/operate/handlers.py --verbose
+
+# kopf
+RUN pip install kopf requests 
+# kubectl
+RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && chmod +x kubectl && mv kubectl /usr/local/bin/kubectl
+# flux
+RUN curl -s https://fluxcd.io/install.sh | bash
+# helm
+RUN curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && chmod 700 get_helm.sh && ./get_helm.sh
+# bbctl
+RUN curl -Ls https://repo1.dso.mil/big-bang/apps/developer-tools/bbctl/-/releases/1.0.0/downloads/bbctl-1.0.0-linux-arm64.tar.gz | tar -xz && mv bbctl /usr/bin/bbctl
+# bbctl runtime setup
+RUN git clone https://repo1.dso.mil/big-bang/bigbang.git /root/repos/bigbang
+
+ENTRYPOINT ["kopf", "run", "/src/operate/handlers.py", "--verbose"]
--- operate/handlers.py
+++ operate/handlers.py
@@ -1,9 +1,74 @@
 import kopf
 import subprocess
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import yaml
+
 
 print("running")
+GITEA_NAMESPACE = "default"
+BIGBANG_OPERATOR_NAMESPACE="default"
+
+def main():
+    install_flux()
+    bootstrap_token = install_gitea()
+    flux_bootstrap(bootstrap_token)
+    # need something like if secret exists then use it to flux_bootstrap otherwise continue? or maybe we need to make sure gitea is removed each time we are initing - prolly better
+    # subprocess.run(["kubectl", "create", "secret", "generic", "--from-literal", f"bootstrap-token={bootstrap_token}", "-n", f"{BIGBANG_OPERATOR_NAMESPACE}", "bootstrap-token"])
+    
+def install_flux():
+    subprocess.run(["flux", "install"])
+
+def install_gitea():
+    
+    subprocess.run(["helm", "repo", "add", "gitea-charts", "https://dl.gitea.com/charts/"])
+    subprocess.run(["helm", "install", "-n", f"{GITEA_NAMESPACE}", "gitea", "gitea-charts/gitea", "-f", "/src/operate/gitea/values.yaml", "--wait"])
+    gitea_svc_endpoint = f"http://gitea-http.{GITEA_NAMESPACE}.svc.cluster.local:3000"
+    url = f"{gitea_svc_endpoint}/api/v1/users/admin/tokens"
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "name": "bootstrap-token",
+        "scopes": [
+            "read:repository",
+            "write:repository",
+            "write:user",
+        ]
+    }
+    auth = HTTPBasicAuth('admin', 'adminpassword')
+    response = requests.post(url, headers=headers, json=data, auth=auth)
+    print(response.json())
+    try:
+        bootstrap_token = response.json()['sha1']
+    except:
+        print("error getting bootstrap token")
+        print(response.text)
+        raise Exception("error getting bootstrap token")
+
+    # create a gitops project for flux bootstrapping
+    url = f"{gitea_svc_endpoint}/api/v1/user/repos"
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {bootstrap_token}"}
+    data = {
+        "name": "flux",
+    }
+    response = requests.post(url, headers=headers, json=data)
+    print(response.json())
+
+    return bootstrap_token
+
+def flux_bootstrap(bootstrap_token: str):
+    subprocess.run(["flux", "bootstrap", "git", "--url", f"http://gitea-http.{GITEA_NAMESPACE}.svc.cluster.local:3000/admin/flux.git", "--username=admin", f"--password={bootstrap_token}", "--allow-insecure-http=true", "--token-auth=true", "--branch=main"])    
 
 @kopf.on.create('bigbang')
-def install_flux(spec, name, meta, status, **kwargs):
-    print("bigbang resources created! installing flux")
-    subprocess.run(["kubectl", "apply", "-f", "https://github.com/fluxcd/flux2/releases/latest/download/install.yaml"])
+def install_bigbang(spec, name, meta, status, **kwargs):
+    print("[INFO] BigBang resources created! running bbctl")
+    # we can do tons of stuff here and above in the deployment for example we can grab all sorts of information like:
+    # running resources | running applications in namespaces | cluster load | available cloud resources | cluster type
+
+    # the information can then be used to either optimize the bigbang install, be opinionated about resource settings, handle upgrades etc. very powerful.
+    subprocess.run(["bbctl", "preflight-check"])
+
+    subprocess.run(["bbctl", "deploy", "bigbang"])
+
+
+main()
